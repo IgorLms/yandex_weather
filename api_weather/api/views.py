c4ed596e1d40c4bcf08a8d0cce397eb376b62473
Igor <sigor97@mail.ru>
2024-01-21 19:32:24 +0300

    Исправил GET запрос для API

--- api_weather/api/views.py
+++ api_weather/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
+from rest_framework.exceptions import ParseError
 
 from api.services.yandex_api.yandex_weather_api import WeatherYandexApi
 
@@ -8,4 +9,7 @@ class WeatherCity(RetrieveAPIView):
     """Передача данных о погоде с Yandex-API по названию города"""
 
     def get(self, request, *args, **kwargs):
-        return Response(WeatherYandexApi(kwargs['city']).get_request())
+        if 'city' in self.request.query_params:
+            return Response(WeatherYandexApi(self.request.query_params.get('city')).get_request())
+
+        raise ParseError(detail='Нет параметров города')
